6295200ed7a01a162781bd7eefec5b103c721925
Aleksandra Sośnierz <olaso3112@gmail.com>
2022-01-08 15:24:44 +0100

    Initial commit

---
+++ src/cku/sopot/TrojkatBokiTest.java
@@ -0,0 +1,47 @@
+package cku.sopot;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TrojkatBokiTest {
+
+    @Test
+    void bokANieMozeBycUjemny() {
+        TrojkatBoki trojkatBoki = new TrojkatBoki(3,4,5);
+        trojkatBoki.setBokA(-3);
+        Assertions.assertEquals(3, trojkatBoki.getBokA());
+    }
+    @Test
+    void bokBNieMozeBycUjemny() {
+        TrojkatBoki trojkatBoki = new TrojkatBoki(3,4,5);
+        trojkatBoki.setBokB(-4);
+        Assertions.assertEquals(4, trojkatBoki.getBokB());
+    }
+    @Test
+    void bokCNieMozeBycUjemny() {
+        TrojkatBoki trojkatBoki = new TrojkatBoki(3,4,5);
+        trojkatBoki.setBokC(-5);
+        Assertions.assertEquals(5, trojkatBoki.getBokC());
+    }
+    @Test
+    void wartosciWKonstruktorzeNieMogaBycUjemne(){
+        TrojkatBoki trojkatBoki = new TrojkatBoki(-3,-4,-5);
+        Assertions.assertEquals(3, trojkatBoki.getBokA());
+        Assertions.assertEquals(4, trojkatBoki.getBokB());
+        Assertions.assertEquals(5, trojkatBoki.getBokC());
+    }
+
+    @Test
+    void poprawneObliczanieObwodu() {
+        TrojkatBoki trojkatBoki = new TrojkatBoki(-3, 4, -5);
+        Assertions.assertEquals(12, trojkatBoki.obliczObwod());
+    }
+
+    @Test
+    void poprawneObliczaniePola() {
+        TrojkatBoki trojkatBoki = new TrojkatBoki(3,-4,-5);
+        Assertions.assertEquals(6, trojkatBoki.obliczPole());
+    }
+}
